49ad6ea8e777c41adcf3cf70798e3b10af50935b
Adepoju Lateef <badepojulateef@gmail.com>
2023-04-24 11:54:44 +0100

    deleted some repos

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,7 +0,0 @@
-# This puppet manifest makes changes to our configuration file
-
-
-exec {'Configure file':
-  command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path    => '/etc/ssh/ssh_config',
-}
