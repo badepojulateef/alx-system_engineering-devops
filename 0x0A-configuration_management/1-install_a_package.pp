b0300616dcb1b3721b6736ba38ffeabcaa1ef965
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-24 05:43:57 +0100

    added task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# This module installs a flask module
+
+package { 'pthon3-pip':
+	ensure => installed,
+}
+
+exec { 'install_flask':
+	command => '/usr/bin/pip3 install Flask==2.1.0',
+	unless => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
