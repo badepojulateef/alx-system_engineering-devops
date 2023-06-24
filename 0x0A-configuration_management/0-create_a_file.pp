a0755045fbeca8253b41fdf230b6954f6b953918
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-24 05:40:15 +0100

    updated task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,19 +1,17 @@
-class school_module::school_file {
-	# Define variables for file attributes
-	$filepath = '/tmp/school'
-	$filemode = '0744'
-	$fileowner = 'www-data'
-	$filegroup = 'www-data'
-	$filecontent = 'I love puppet\n'
-}
+# This module creates a file
+	
+$filepath = '/tmp/school'
+$filemode = '0744'
+$fileowner = 'www-data'
+$filegroup = 'www-data'
+$filecontent = 'I love puppet\n'
+
 
 # Create the file resources
 file { $filepath:
 	ensure => file,
 	mode => $filemode,
-	owner => $fileowner
-	group => $filegroup
-	content => $filecontent
+	owner => $fileowner,
+	group => $filegroup,
+	content => $filecontent,
 }
-
-include school_module::school_file
