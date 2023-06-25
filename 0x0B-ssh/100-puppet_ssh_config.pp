9d54dc6792e1e1e638c98a822cbd344e9bae5b42
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-25 22:31:15 +0100

    Update task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # This puppet manifest makes use of puppet to
 # make changes to our configuration file
 
-exec {
+exec { 'Configure file': 
   command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
   path => '/etc/ssh/ssh_config',
 }
