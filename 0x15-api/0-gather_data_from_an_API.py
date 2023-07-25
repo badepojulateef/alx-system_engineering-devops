afc3651a869aeeb825549b2219cc6567c1310622
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-25 05:37:10 +0100

    added task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""  returns information about his/her TODO list progress """
+
+import sys
+import requests
+
+
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieves and displays the TODO list progress
+    of a given employee from a REST API.
+
+    Parameters:
+        employee_id (int): The unique ID of the employee whose TODO
+                           progress is to be fetched.
+
+    Returns:
+        None: This function displays the employee's TODO lis
+                progress on the standard output.
+
+    Raises:
+        ValueError: If the provided employee_id is not an integer.
+
+    """
+
+    # Replace this with the actual API URL
+    base_url = "https://jsonplaceholder.typicode.com/"
+
+    users = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(base_url + "todos", params={"userId": sys.argv[1]})\
+                    .json()
+
+    print(users)
+    print('-------------')
+    print(todos)
+    # Fetch the employee's TODO list progress from the API
+
+    # data = users.json()
+
+    # Extract relevant information from the response
+    employee_name = users["name"]
+    done_tasks = [task for task in todos if task["completed"]]
+    num_done_tasks = len(done_tasks)
+    total_tasks = len(todos)
+
+    # Display the information in the specified format
+    print(f"Employee {employee_name} is done\
+            with tasks({num_done_tasks}/{total_tasks}):")
+
+    # Display the titles of completed tasks
+    for task in done_tasks:
+        print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Invalid employee ID. Please enter an integer.")
+        sys.exit(1)
+
+    get_employee_todo_progress(employee_id)
