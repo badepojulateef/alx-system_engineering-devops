09266761b8add572c68452d4f198ce6f77406c14
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-27 01:15:13 +0100

    Added task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""
+Writes information about his/her TODO list progress
+"""
+
+import csv
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieves and displays the TODO list progress
+    of a given employee from a REST API.
+
+    Parameters:
+        employee_id (int): The unique ID of the employee whose TODO
+                           progress is to be fetched.
+
+    Returns:
+        None: This function displays the employee's TODO lis
+                progress on the standard output.
+
+    Raises:
+        ValueError: If the provided employee_id is not an integer.
+
+    """
+
+    # Replace this with the actual API URL
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, sys.argv[1])
+    todos_url = "{}/todos/".format(base_url)
+
+    # Extract relevant information from the response
+    employee = requests.get(user_url).json()
+    todos = requests.get(todos_url, params={"userId": sys.argv[1]}).json()
+
+    # Extract the employee name
+    employee_name = employee["name"]
+
+    completed_tasks = [task for task in todos if task["completed"]]
+    len_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos)
+
+    # Display the information in the specified format
+    first_line = "Employee {} is done with tasks({}/{}):"\
+                 .format(employee_name, len_completed_tasks, total_tasks)
+
+    # Print the first_line
+    # print(first_line)
+
+    # Display the titles of completed tasks
+    for task in completed_tasks:
+        filename = "{}.csv".format(employee_id)
+
+    # open new file for writing - will erase file if it already exists
+    with open(filename, mode="w", newline='') as csvfile:
+
+        for task in completed_tasks:
+            employee_id = task["userId"]
+            task_completed_status = task["completed"]
+            task_title = task["title"]
+
+            res = '{},{},{},{}\n'\
+                  .format(
+                      employee_id,
+                      employee_name,
+                      task_completed_status,
+                      task_title
+                  )
+
+            print(res)
+            csvfile.write(res)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Invalid employee ID. Please enter an integer.")
+        sys.exit(1)
+
+    get_employee_todo_progress(employee_id)
