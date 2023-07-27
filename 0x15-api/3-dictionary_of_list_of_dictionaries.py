608af8f7ed76ee14c8d3be60bc5f5ef759dbe598
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-27 05:26:34 +0100

    Updated task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,66 +8,59 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
+def get_all_employee_todo_progress():
     """
     Retrieves and displays the TODO list progress
     of a given employee from a REST API.
 
     Parameters:
-        employee_id (int): The unique ID of the employee whose TODO
-                           progress is to be fetched.
 
     Returns:
         None: This function displays the employee's TODO lis
                 progress on the standard output.
 
     Raises:
-        ValueError: If the provided employee_id is not an integer.
+        ValueError: If the provided script is not an integer.
 
     """
 
     # Replace this with the actual API URL
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, sys.argv[1])
-    todos_url = "{}/todos/".format(base_url)
+    users_url = "{}/users".format(base_url)
 
-    # Extract relevant information from the response
-    employee = requests.get(user_url).json()
-    todos = requests.get(todos_url).json()
+    employees = requests.get(users_url).json()
 
-    # Extract the employee name
-    employee_name = employee["name"]
+    tasks_dict = {}
 
-    completed_tasks = [task for task in todos if task["completed"]]
-    len_completed_tasks = len(completed_tasks)
-    total_tasks = len(todos)
+    for employee in employees:
+        # print(employee)
+        # print("---------")
+        employee_id = employee['id']
+        # print("---------")
+        todos_url = "{}/{}/todos".format(users_url, employee['id'])
+        # print(todos_url)
+        employee_todos = requests.get(todos_url).json()
+
+        tasks_dict[employee_id] = []
+
+        for task in employee_todos:
+            tasks_dict[employee_id].append({
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": employee["username"]
+            })
+
+        # print(tasks_dict)
 
     # open new file for writing - will erase file if it already exists
-    tasks_dict = {employee_id: []}
-
-    for task in todos:
-        tasks_dict[employee_id].append({
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": employee["username"]
-        })
-
-    # print(tasks_dict)
-
-    with open(str("todo_all_employees.json", mode="w", newline='') as jsonfile:
-        json.dump(tasks_dict, jsonfile)
+    with open(str("todo_all_employees.json"), mode="w", newline='') as file:
+        json.dump(tasks_dict, file)
 
 
 if __name__ == "__main__":
     """ Records all tasks from all employees """
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+    if len(sys.argv) != 1:
+        print("Usage: python script.py")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Invalid employee ID. Please enter an integer.")
-        sys.exit(1)
-
-    get_employee_todo_progress(employee_id)
+    get_all_employee_todo_progress()
