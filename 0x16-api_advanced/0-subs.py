91835ffc61eac480eb3babfc5bd1101d80ec5d18
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-08 12:24:30 +0100

    Added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieve the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subscribers for the subreddit. Returns 0 if the subreddit is invalid or an error occurs.
+    """
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {"User-Agent": "CustomBot"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    try:
+        res = requests.get(url)
+        data = res.json()
+        if not data:
+            return 0
+        if "data" not in data:
+            return 0
+        return data["data"]["subscribers"]
+    except requests.RequestException as e:
+        return 0
