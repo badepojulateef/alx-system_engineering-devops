913bd8a3f8046fcdf4f35af2e051afbd78e55ac2
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-10 03:17:58 +0100

    Added task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,20 @@
+# Define the path to the file to edit
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+# Define a variable for the desired repalcement
+$old_string = 'phpp'
+$new_string = 'php'
+
+# Check if the file exists before proceeding
+if !file_exists($file_to_edit) {
+  fail("File ${file_to_edit} doest not exist")
+}
+
+# Define the exec resource to replace
+# the line using the 'sed' command
+exec {'fix bug':
+  command     => "sed -i 's/${old_string}/${new_string}/g' ${file_to_edit}",
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+  subscribe   => File[$file_to_edit]
+}
