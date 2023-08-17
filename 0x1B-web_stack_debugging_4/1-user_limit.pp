0ef805b668881fa1d50824e78aefbd8ec75b2b71
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-17 18:09:22 +0100

    added task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# This script changes the os config to allow 'holberton' user to login
+
+exec {'increase hard limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec {'increase soft limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
