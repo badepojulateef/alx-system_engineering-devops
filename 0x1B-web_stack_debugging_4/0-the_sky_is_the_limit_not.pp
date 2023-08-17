ab9f0f59b320b971dd0434d7b57a044b351788b5
Adepoju Lateef <badepojulateef@gmail.com>
2023-08-17 17:26:54 +0100

    added task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# This script increases the load of a server
+
+exec {'increase-load':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+
+# Restart Nginx
+-> exec {
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
